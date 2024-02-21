3906bf915f91f38b63354b98a557e9d7879d7746
LoshakovaAnna <ann.loshakova.111@gmail.com>
2024-02-21 15:32:53 +0300

    [BE] add index.js and employee router/controller/model

---
+++ be/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors')
+
+const employeeRouter = require('./src/routes/employee-router');
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use((req, res) => {
+    res.setHeader(
+        'Content-type', 'application/json'
+    );
+    res.send = (data) => {
+        res.end(JSON.stringify(data));
+    };
+    req.next();
+});
+app.use('/api/v1', employeeRouter);
+const PORT = process.env.PORT || 5000;
+
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://userAn:****@cluster0.i84rot5.mongodb.net/myDB?retryWrites=true&w=majority');
+        app.listen(5000, () => console.log(`Server started on PORT ${5000}`));
+    } catch (e) {
+        console.log(e);
+        console.log('Connect to DB is failed.');
+    }
+}
+
+start();
---
+++ be/src/controllers/employee-controller.js
@@ -0,0 +1,98 @@
+const EmployeeModel = require('../models/employee-model');
+
+const transformEmployeeFromBDToSendFormat = (employeeDB) => {
+    const {name, lastName, patronymic, title, _id: id} = employeeDB;
+    return {id, name, lastName, patronymic, title};
+};
+const getEmployees = async (req, res) => {
+    let employees = req.params.id
+        ? EmployeeModel.findById(req.params.id)
+            .then(employees => (transformEmployeeFromBDToSendFormat(employees)))
+        : EmployeeModel.find()
+            .then(employees => (employees.map(empl => (transformEmployeeFromBDToSendFormat(empl)))));
+    employees
+        .then(
+            data => {
+                res.send(data);
+            }
+        )
+        .catch(error => {
+            console.log(`find employee(s) is failed, obj=${req.params.id}`);
+            res.status(500).send({message: 'Find employee(s) is failed.'});
+        });
+}
+
+const createEmployee = async (req, res) => {
+    if (!req.body) {
+        return res.status(400).send({message: 'No body found.'});
+    }
+    const {name, lastName, patronymic, title} = req.body;
+    if (!name || !lastName || !patronymic || !title) {
+        return res.status(404)
+            .send({message: 'Failed validation: empty required field(s). Check name, lastName, patronymic, title '});
+    }
+
+    EmployeeModel.create({name, lastName, patronymic, title})
+        .then(
+            employee => {
+                res.send(transformEmployeeFromBDToSendFormat(employee));
+            }
+        )
+        .catch(error => {
+            console.log(`create employee is failed, obj=${req.body ? JSON.stringify(req.body) : req.body}`);
+            res.status(500).send({message:'create employee is failed'});
+        });
+}
+
+const updateEmployee = async (req, res) => {
+    const id = req?.params?.id;
+    if (!id) {
+        return res.status(400).send({message: 'No id found.'});
+    }
+    const {name, lastName, patronymic, title} = req?.body;
+    if (!name || !lastName || !patronymic || !title) {
+        return res.status(404).send(
+            {message: 'Failed validation: empty required field(s). Check name, lastName, patronymic, title '}
+        )
+    }
+
+    EmployeeModel.findByIdAndUpdate(id, {name, lastName, patronymic, title})
+        .then(
+            employee => {
+                res.send('ok');
+            }
+        )
+        .catch(error => {
+            console.log(`update employee is failed, id=${id}, obj=${req.body ? JSON.stringify(req.body) : req.body}`);
+            res.status(500).send({message: 'update employee is failed'});
+        });
+}
+
+const deleteEmployee = async (req, res) => {
+    const id = req?.params?.id;
+    if (!id) {
+        return res.status(400).send({message: 'no id found'});
+    }
+
+    EmployeeModel.findByIdAndDelete(id)
+        .then(
+            d => {
+                if (!d) {
+                    return res.status(404).send({message:'no employee found'});
+                }
+                res.status(200).send(d);
+            }
+        )
+        .catch(error => {
+            console.log(`findByIdAndDelete employee is failed, id=${id}`);
+            res.status(500).send({message:'findByIdAndDelete employee is failed!'});
+        });
+
+}
+
+module.exports = {
+    getEmployees,
+    createEmployee,
+    deleteEmployee,
+    updateEmployee
+};
