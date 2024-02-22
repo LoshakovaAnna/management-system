ccc9158c6e8bf6e2a8087defd09b3aaea6c0f1cb
LoshakovaAnna <ann.loshakova.111@gmail.com>
2024-02-22 23:50:15 +0300

    [BE] improve get employee: add pagination and sort

--- be/src/controllers/employee-controller.js
+++ be/src/controllers/employee-controller.js
@@ -19,6 +19,34 @@ const getEmployees = async (req, res) => {
         });
 }
 
+const getEmployeesPagination = async (req, res) => {
+    let count = 0;
+    EmployeeModel.estimatedDocumentCount()
+        .then(
+            data => {
+                count = data;
+                if (!count) {
+                    return res.status(200).send([])
+                }
+                let {sort, sortDirection, limit, page} = req.query ? req.query : {limit: 5, page: 0};
+                const skip = limit * page;
+                sortDirection = !sortDirection ? 1 : +sortDirection;
+                return EmployeeModel.find().sort(sort ? {[sort]: sortDirection} : null).skip(skip).limit(limit);
+            }
+        )
+        .then(employees => (employees.map(empl => (transformToSendFormat(empl)))))
+        .then(employees => {
+            res.send({
+                employees,
+                total: count
+            });
+        })
+        .catch(() => {
+            console.log(`find employee(s) is failed, q=${req.query}`);
+            res.status(500).send({message: 'Find employee(s) is failed.'});
+        });
+}
+
 const createEmployee = async (req, res) => {
     if (!req.body) {
         return res.status(400).send({message: 'No body found.'});
@@ -37,7 +65,7 @@ const createEmployee = async (req, res) => {
         )
         .catch(error => {
             console.log(`create employee is failed, obj=${req.body ? JSON.stringify(req.body) : req.body}`);
-            res.status(500).send({message:'create employee is failed'});
+            res.status(500).send({message: 'create employee is failed'});
         });
 }
 
@@ -75,20 +103,21 @@ const deleteEmployee = async (req, res) => {
         .then(
             d => {
                 if (!d) {
-                    return res.status(404).send({message:'no employee found'});
+                    return res.status(404).send({message: 'no employee found'});
                 }
                 res.status(200).send(d);
             }
         )
         .catch(error => {
             console.log(`findByIdAndDelete employee is failed, id=${id}`);
-            res.status(500).send({message:'findByIdAndDelete employee is failed!'});
+            res.status(500).send({message: 'findByIdAndDelete employee is failed!'});
         });
 
 }
 
 module.exports = {
     getEmployees,
+    getEmployeesPagination,
     createEmployee,
     deleteEmployee,
     updateEmployee
